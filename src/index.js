e5391b6a4b6df4cb2c4927de83ca403d90c4d8ea
Celshya D <59332123+celshya@users.noreply.github.com>
2023-03-18 19:20:33 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const app = require("./app");
+const config = require("./config/config");
+
+let server;
+mongoose
+  .connect(config.mongoose.url,config.mongoose.options)
+  .then(() => {
+    console.log("Connected to DB at", config.mongoose.url)
+    server=app.listen(config.port, () => {
+        console.log("Server Listening at", config.port);
+      });
+})
+  .catch((error) => console.log("Failed to connect to DB\n", error));
+// TODO: CRIO_TASK_MODULE_UNDERSTANDING_BASICS - Create Mongo connection and get the express app to listen on config.port
